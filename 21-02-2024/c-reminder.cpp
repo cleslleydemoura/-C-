7e15ad3b4b256c0c5cd68a8e299fcbbc068ef36e
Cleslley de Moura <100368699+cleslleydemoura@users.noreply.github.com>
2024-02-21 10:49:48 -0300

    21022024 - imc calculator

--- 21-02-2024/c-reminder.cpp
+++ 21-02-2024/c-reminder.cpp
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
-// CÁLCULO IMC
-
 int main() {
     float peso, altura, imc;
     printf("Digite seu peso: ");
-    scanf("%f", &peso); // o & aponta para qual variável está sendo atribuída este valor.
+    scanf("%f", &peso);
     printf("Digite sua altura: ");
     scanf("%f", &altura);
     
     imc = peso / (altura * altura);
     
     printf("Seu IMC é: %.2f", imc); // reminder: não se coloca & na frente da variável como se coloca dentro do recebimento da variável no scanf.
+
+    if (imc < 20)
+     printf(" (Abaixo do peso).");
+  else if ((imc >= 20) && (imc < 25))
+          printf(" (Peso normal).");
+       else if ((imc >= 25) && (imc < 30))
+               printf(" (Acima do peso).");
+            else if ((imc >= 30) && (imc < 34))
+                    printf(" (Obesidade).");
+		 else
+		    printf(" (Muito Obeso).");
 }
